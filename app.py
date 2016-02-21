51a333099749504c29081543430cb50f62a75950
Cody Bouscaren <codybousc@gmail.com>
2016-02-21 18:45:07 -0500

    Basic db creation/rendering

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, g
+import sqlite3
 
 app = Flask(__name__)
 
+app.database = "sample.db"
+
 @app.route('/')
 def home():
-    return "Hello, world!"
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    return render_template('index.html', posts=posts)
 
 @app.route('/welcome')
 def welcome():
@@ -19,6 +25,10 @@ def login():
         else:
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+
+def connect_db():
+    return sqlite3.connect(app.database)
 
 
 if __name__ == '__main__':
